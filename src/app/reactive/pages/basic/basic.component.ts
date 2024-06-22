54c0aa7cfb8691b048c56681b929918e27e7fd0d
Uriel Lozano <strike_ls@hotmail.com>
2024-06-21 21:18:38 -0600

    Connect form with formGroup angular

--- src/app/reactive/pages/basic/basic.component.ts
+++ src/app/reactive/pages/basic/basic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'reactive-basic',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
 
+  // Form group
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  private formBuilder = inject(FormBuilder);
+  public myForm: FormGroup = this.formBuilder.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave(): void {
+    console.log('Form value:', this.myForm.value);
   }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BasicComponent } from './pages/basic/basic.component';
+import { DynamicComponent } from './pages/dynamic/dynamic.component';
+import { SwitchesComponent } from './pages/switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicComponent,
+    DynamicComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
