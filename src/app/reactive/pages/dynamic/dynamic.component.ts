3c12446ddeeeb9a6355672b7758574da1955ce4b
Uriel Lozano <strike_ls@hotmail.com>
2024-06-22 23:49:16 -0600

    Create dynamic form with FormArray

--- src/app/reactive/pages/dynamic/dynamic.component.ts
+++ src/app/reactive/pages/dynamic/dynamic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-dynamic',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComponent implements OnInit {
 
+  private formBuilder = inject(FormBuilder);
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', Validators.required, Validators.minLength(3)],
+    favoriteGames: this.formBuilder.array([
+      ['Halo', Validators.required],
+      ['Gears of War', Validators.required],
+      ['Forza Horizon', Validators.required]
+    ])
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get favoriteGames(): FormArray {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void{
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Form value:', this.myForm.value);
+    this.myForm.reset();
   }
 
 }
