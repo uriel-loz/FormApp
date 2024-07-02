0e93774cf16b3b4d0fce685f6b0914ab630d347f
Uriel Lozano <strike_ls@hotmail.com>
2024-07-02 11:27:37 -0600

    Validations for auth component

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  private formBuilder = inject(FormBuilder);
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    passwordConfirmation: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit() {
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
   }
 
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
 }
